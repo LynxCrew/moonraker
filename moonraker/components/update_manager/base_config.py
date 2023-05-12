782a9d4fea98901bd15334289dc515d492350e24
LastZeanon <lastzeanon@gmail.com>
2023-05-12 18:56:50 +0200

    initial

--- moonraker/components/update_manager/base_config.py
+++ moonraker/components/update_manager/base_config.py
@@ -23,7 +23,7 @@ KLIPPER_DEFAULT_EXEC = os.path.expanduser("~/klippy-env/bin/python")
 
 BASE_CONFIG: Dict[str, Dict[str, str]] = {
     "moonraker": {
-        "origin": "https://github.com/arksine/moonraker.git",
+        "origin": "https://github.com/Zeanon/moonraker.git",
         "requirements": "scripts/moonraker-requirements.txt",
         "venv_args": "-p python3",
         "install_script": "scripts/install-moonraker.sh",
@@ -33,8 +33,8 @@ BASE_CONFIG: Dict[str, Dict[str, str]] = {
         "managed_services": "moonraker"
     },
     "klipper": {
-        "moved_origin": "https://github.com/kevinoconnor/klipper.git",
-        "origin": "https://github.com/Klipper3d/klipper.git",
+        "moved_origin": "https://github.com/Zeanon/klipper.git",
+        "origin": "https://github.com/Zeanon/klipper.git",
         "requirements": "scripts/klippy-requirements.txt",
         "venv_args": "-p python2",
         "install_script": "scripts/install-octopi.sh",
