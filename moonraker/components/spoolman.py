8b219bf54c257580ab55bec6c041e9145c57c843
Zeanon <thezeanon@gmail.com>
2024-01-15 21:04:51 +0100

    Update spoolman.py

--- moonraker/components/spoolman.py
+++ moonraker/components/spoolman.py
@@ -198,6 +198,7 @@ class SpoolManager:
             body=body,
         )
 
+        logging.info(response.json().get("message"))
         if (response._code == 404
                 and response.json().get("message").startswith("No spool with ID ")
                 and response.json().get("message").endswith(" found")):
